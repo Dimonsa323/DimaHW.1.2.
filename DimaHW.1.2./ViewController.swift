22b25c92bf747bf84d5ab7206ad38985313c7bf2
Dima Hubenya <dima96gubenya@gmail.com>
2022-06-19 17:14:33 +0300

    Initial Commit

---
+++ DimaHW.1.2./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DimaHW.1.2.
+//
+//  Created by Дима Губеня on 19.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
