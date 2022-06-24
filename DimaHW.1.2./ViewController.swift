fc77f2f8e10819e439dd45fbc13f39db51de716d
Dima Hubenya <dima96gubenya@gmail.com>
2022-06-24 17:01:11 +0300

    Второй комит

--- DimaHW.1.2./ViewController.swift
+++ DimaHW.1.2./ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
+enum TrafficLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    private var trafficLight = TrafficLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 20
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
+        print("размер стороны доступны из viewWillLayoutSubviews", redLight.frame.height)
     }
-
-
+    override func viewDidLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = yellowLight.frame.width / 2
+    }
+    
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        
+    }
 }
 
